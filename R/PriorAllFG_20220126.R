b68cc8ddafc69e701dc9c1dd0fa53684a8411091
Geraldine Derroire <geraldine.derroire@cirad.fr>
2022-01-26 22:44:54 -0300

    Add prior files and helpfile + some changes on the vignette

---
+++ R/PriorAllFG_20220126.R
@@ -0,0 +1,21 @@
+#' PriorAllFG_20220126
+#'
+#' Prior files resulting from the work of Jean-Maurice MadKaud (see Vignette),
+#' for which the botanical and vernacular names have been checked
+#' and updated on the 26/01/2022.
+#' This prior file correspond to the prior file called "Probabilités Guyane
+#' (sans erreur de relevé, facteur zone)" in Madkaud's report.
+#'
+#' @usage data(PriorAllFG_20220126)
+#'
+#' @format A data.frame with 1657 rows and 197 variables:
+#' \describe{
+#'   \item{Family}{Family name (character)}
+#'   \item{Genus}{Genus name (character)}
+#'   \item{Species}{Species name (character)}
+#'   \item{PresentInGuyaFor}{Is the taxon present in Guyafor database? (bolean)}
+#'    ... vernacular names (0 if no possible association, 1 if possible
+#'    association according to expert knowledge)
+#' }
+
+"PriorAllFG_20220126"
---
+++ R/PriorParacouNew_20220126.R
@@ -0,0 +1,21 @@
+#' PriorParacouNew_20220126
+#'
+#' Prior files resulting from the work of Jean-Maurice MadKaud (see Vignette),
+#' for which the botanical and vernacular names have been checked
+#' and updated on the 26/01/2022.
+#' This prior file correspond to the prior file called
+#' "Probabilités Paracou (avec erreur de relevé, facteur temps (équipe))" in Madkaud's report.
+#'
+#' @usage data(PriorParacouNew_20220126)
+#'
+#' @format A data.frame with 661 rows and 196 variables:
+#' \describe{
+#'   \item{Family}{Family name (character)}
+#'   \item{Genus}{Genus name (character)}
+#'   \item{Species}{Species name (character)}
+#'   \item{PresentInGuyaFor}{Is the taxon present in Guyafor database? (bolean)}
+#'    ... vernacular names (0 if no possible association, 1 if possible
+#'    association according to expert knowledge)
+#' }
+
+"PriorParacouNew_20220126"
